ea7bedc9588100a0fe745a7d855f8dd09b06da1e
Yexue <ofsanker@qq.com>
2025-07-11 14:45:17 +0800

    fix 去除其余小程序的库，去除其他文件

--- src/composables/useSystemQuery.ts
+++
@@ -1,9 +0,0 @@
-import {useQuery} from "@tanstack/vue-query";
-import {getBanner} from "@/api";
-
-export const useBannerQuery = (module: Ref<number> | number) => useQuery({
-  queryKey: ['banner', typeof module === 'number' ? module : module.value],
-  queryFn: () => getBanner(typeof module === 'number' ? module : module.value),
-  select: (data) => data?.records?.pop()?.url || '',
-  enabled: typeof module === 'number' ? true : module.value !== undefined
-})
